3b49b8182c3a510469153892b6a3ef209346ba7e
EliasMorio <elias14160@gmail.com>
2023-01-19 22:28:30 +0100

    Adding possibility to create a new item

--- src/main/java/edu/spring/td1/controllers/MainController.java
+++ src/main/java/edu/spring/td1/controllers/MainController.java
@@ -4,12 +4,13 @@ import edu.spring.td1.models.Element;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SessionAttributes("items")
 @Controller
+@SessionAttributes("items")
 public class MainController {
 
     @ModelAttribute("items")
@@ -18,12 +19,25 @@ public class MainController {
     }
 
     @RequestMapping(value = "/items", method = {RequestMethod.GET, RequestMethod.POST})
-    public String show(ModelMap model){
-        model.addAttribute("items", getItems());
+    public String show(ModelMap model,
+                       @ModelAttribute("items") List<Element> items){
+        model.addAttribute("items", items);
         return "items/show";
     }
 
+    @GetMapping("/items/new")
+    public String add(){
+        return "items/add";
+    }
 
+    @PostMapping("/items/addNew")
+    public RedirectView store(@RequestParam String name,
+                              @ModelAttribute("items") List<Element> items){
+        Element el = new Element();
+        el.setName(name);
+        items.add(el);
+        return new RedirectView("/items");
+    }
 
 
 
