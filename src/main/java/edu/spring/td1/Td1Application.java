bdbccce896c81fd2817d51d56799679ad9836745
EliasMorio <elias14160@gmail.com>
2023-01-19 15:04:07 +0100

    Adding initial project

---
+++ src/main/java/edu/spring/td1/Td1Application.java
@@ -0,0 +1,13 @@
+package edu.spring.td1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td1Application.class, args);
+    }
+
+}
