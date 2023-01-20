a3738186569bb716572e938d3982db31e4025359
EliasMorio <elias14160@gmail.com>
2023-01-20 12:23:35 +0100

    Adding basic tests to use with GitHub actions

--- src/test/java/edu/spring/td1/Td1ApplicationTests.java
+++ src/test/java/edu/spring/td1/Td1ApplicationTests.java
@@ -1,13 +1,42 @@
 package edu.spring.td1;
 
+import edu.spring.td1.models.Element;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Td1ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("categories"))
+                .andExpect(model().attributeExists("items"));
+    }
+
+    @Test
+    void testItems() throws Exception {
+        mockMvc.perform(get("/items"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("items"));
     }
 
 }
