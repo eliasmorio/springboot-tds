34206f67d6af73ed55b7149f5f11824a4f6807c8
EliasMorio <elias14160@gmail.com>
2023-01-27 09:35:17 +0100

    Adding Item model

---
+++ src/main/kotlin/edu/spring/td1/models/Item.kt
@@ -0,0 +1,16 @@
+package edu.spring.td1.models
+
+class Item {
+    var name: String? = null
+    var evaluation = 0
+
+    override fun equals(obj: Any?): Boolean {
+        return if (obj is Item) {
+            name == obj.name
+        } else false
+    }
+
+    override fun hashCode(): Int {
+        return name?.hashCode() ?: 0
+    }
+}
