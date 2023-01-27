65425cf044778249357942d857425d4fa4622ac0
EliasMorio <elias14160@gmail.com>
2023-01-27 09:28:31 +0100

    Adding initial project + config of application.properties

---
+++ src/main/kotlin/edu/spring/td1/SpringbootApplication.kt
@@ -0,0 +1,11 @@
+package edu.spring.td1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootApplication>(*args)
+}
