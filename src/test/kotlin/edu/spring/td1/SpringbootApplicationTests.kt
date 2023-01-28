68d3f493871e460ca85252004429347f527f2d9b
EliasMorio <elias14160@gmail.com>
2023-01-28 19:24:38 +0100

    Adding basic test for index

--- src/test/kotlin/edu/spring/td1/SpringbootApplicationTests.kt
+++ src/test/kotlin/edu/spring/td1/SpringbootApplicationTests.kt
@@ -1,13 +1,31 @@
 package edu.spring.td1
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringbootApplicationTests {
+
+	@Autowired
+	var mvc: MockMvc? = null
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun testIndex() {
+		mvc!!.perform(get("/"))
+				.andExpect(status().isOk)
+				.andExpect(view().name("items/show"))
+				.andExpect(model().attributeExists("items"))
+				.andExpect(model().attributeExists("categories"))
 	}
 
 }
