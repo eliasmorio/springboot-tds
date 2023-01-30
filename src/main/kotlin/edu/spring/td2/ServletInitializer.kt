d5884bc5d38baf0da85e68c1d527f9068ae792b1
EliasMorio <elias14160@gmail.com>
2023-01-30 08:06:25 +0100

    adding file project td2

---
+++ src/main/kotlin/edu/spring/td2/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package edu.spring.td2
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Td2Application::class.java)
+    }
+
+}
---
+++ src/main/kotlin/edu/spring/td2/Td2Application.kt
@@ -0,0 +1,11 @@
+package edu.spring.td2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Td2Application
+
+fun main(args: Array<String>) {
+    runApplication<Td2Application>(*args)
+}
