59f708f585bc91379e7abb8c8b658d8acb573e0f
EliasMorio <elias14160@gmail.com>
2023-02-06 10:10:56 +0100

    feat(group): add new group

--- src/main/kotlin/edu/spring/td2/controllers/GroupController.kt
+++ src/main/kotlin/edu/spring/td2/controllers/GroupController.kt
@@ -1,11 +1,15 @@
 package edu.spring.td2.controllers
 
+import edu.spring.td2.entities.Group
+import edu.spring.td2.entities.User
 import edu.spring.td2.repositories.GroupRepository
 import edu.spring.td2.services.UIMessage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
@@ -31,4 +35,19 @@ class GroupController {
         model["groups"] = groupRepository.findAll()
         return "groups/index"
     }
+
+    @GetMapping("/new")
+    fun new(model: ModelMap) : String{
+        model["group"] = User()
+        return "groups/new"
+    }
+
+    @PostMapping("/store")
+    fun store(@ModelAttribute group: Group?) : String{
+        if (group != null) {
+            groupRepository.save(group)
+        }
+        return "redirect:/groups"
+    }
+
 }
