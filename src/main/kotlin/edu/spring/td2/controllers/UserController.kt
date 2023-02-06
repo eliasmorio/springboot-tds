fe82f9bc8169285dc448d2ab1c5c2e931c544d3b
EliasMorio <elias14160@gmail.com>
2023-02-06 09:27:35 +0100

    feat(user): display user details

--- src/main/kotlin/edu/spring/td2/controllers/UserController.kt
+++ src/main/kotlin/edu/spring/td2/controllers/UserController.kt
@@ -2,10 +2,12 @@ package edu.spring.td2.controllers
 
 import edu.spring.td2.entities.User
 import edu.spring.td2.repositories.UserRepository
+import edu.spring.td2.services.UIMessage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequestMapping("/users")
@@ -13,6 +15,16 @@ class UserController {
 
     @Autowired
     lateinit var userRepository: UserRepository
+
+    private fun addMsg(resp:Boolean, attrs: RedirectAttributes, title:String, success:String, error:String){
+        if(resp) {
+            attrs.addFlashAttribute("msg",
+                UIMessage.message(title, success))
+        } else {
+            attrs.addFlashAttribute("msg",
+                UIMessage.message(title, error,"error","warning circle"))
+        }
+    }
 
     @GetMapping("", "/")
     fun index(model: ModelMap) : String {
@@ -45,6 +57,19 @@ class UserController {
         if(model["user"] == null)
             return "redirect:/users" //TODO : display error
         return "users/edit"
+    }
+
+    @GetMapping("/display/{id}")
+    fun display(model: ModelMap, @PathVariable id: Int,
+                attrs: RedirectAttributes
+    ) : String{
+        val user =  userRepository.findById(id)
+        if (!user.isPresent) {
+            addMsg(false, attrs, "Erreur", "", "Cette utilisateur n'existe pas")
+            return "redirect:/users"
+        }
+        model["user"] = user.get()
+        return "users/display"
     }
 
 
