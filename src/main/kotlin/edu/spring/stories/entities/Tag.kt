d3dd882b6a8c1d9a440315cd8f65dd956bcdde0f
EliasMorio <elias14160@gmail.com>
2023-03-02 13:38:37 +0100

    fix: add attributes in Tag entity

--- src/main/kotlin/edu/spring/stories/entities/Tag.kt
+++ src/main/kotlin/edu/spring/stories/entities/Tag.kt
@@ -2,11 +2,15 @@ package edu.spring.stories.entities
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.ManyToMany
 
 @Entity
 class Tag {
 
     @Id
     var id: Int? = null
-    var name: String? = null
+    var label: String? = null
+    var color: String? = null
+    @ManyToMany(mappedBy = "tags")
+    var stories: MutableSet<Story> = mutableSetOf()
 }
