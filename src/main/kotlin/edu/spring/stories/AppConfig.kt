d78fed953c36298221bcc207f9c4fadbf5f224f7
EliasMorio <elias14160@gmail.com>
2023-03-07 16:06:26 +0100

    feat: add vue dependency and config

---
+++ src/main/kotlin/edu/spring/stories/AppConfig.kt
@@ -0,0 +1,9 @@
+package edu.spring.stories
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("io.github.jeemv.springboot.vuejs")
+class AppConfig {
+}
