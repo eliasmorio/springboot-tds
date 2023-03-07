7a837485c1db12ec2124ea0e84522d92d8f0cf21
EliasMorio <elias14160@gmail.com>
2023-03-07 15:58:15 +0100

    fix: add JSON ignore to attributes to avoid loop in JSON

--- src/main/kotlin/edu/spring/stories/entities/Story.kt
+++ src/main/kotlin/edu/spring/stories/entities/Story.kt
@@ -1,6 +1,8 @@
 package edu.spring.stories.entities
 
+import com.fasterxml.jackson.annotation.JsonBackReference
 import jakarta.persistence.*
+import org.springframework.data.rest.core.annotation.RestResource
 
 @Entity
 class Story() {
@@ -13,7 +15,9 @@ class Story() {
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Int = 0
     var name: String? = null
-    @ManyToOne
+    @ManyToOne(optional = true)
+    @RestResource(exported = false, rel = "developer", path = "developer")
+    @JsonBackReference
     var developer: Developer? = null
     @ManyToMany
     var tags: MutableSet<Tag> = mutableSetOf()
