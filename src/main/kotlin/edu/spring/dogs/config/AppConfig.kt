b1efa724a50c4f70fb461aa5b94615cfc9b40204
EliasMorio <elias14160@gmail.com>
2023-03-16 15:48:07 +0100

    feat(security): add spring-security dependancy

--- src/main/kotlin/edu/spring/dogs/config/AppConfig.kt
+++ src/main/kotlin/edu/spring/dogs/config/AppConfig.kt
@@ -2,8 +2,15 @@ package edu.spring.dogs.config
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @ComponentScan("io.github.jeemv.springboot.vuejs")
-class AppConfig {
+class AppConfig : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/login").setViewName("/forms/loginForm")
+    }
 }
