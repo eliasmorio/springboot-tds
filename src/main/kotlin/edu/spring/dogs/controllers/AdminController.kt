2577f7ba2e865eae9cd10533f2ff081b97943c40
EliasMorio <elias14160@gmail.com>
2023-03-20 19:50:54 +0100

    fix(admin): edit user

--- src/main/kotlin/edu/spring/dogs/controllers/AdminController.kt
+++ src/main/kotlin/edu/spring/dogs/controllers/AdminController.kt
@@ -24,21 +24,17 @@ class AdminController {
     @Autowired
     lateinit var roleRepository: RoleRepository
 
-    @ModelAttribute("user")
-    fun user(auth: Authentication) = auth
-
-    @ModelAttribute("isAdmin")
-    fun isAdmin(auth: Authentication) = auth.authorities.any { it.authority == "ADMIN" }
-
-    @ModelAttribute("users")
-    fun users(): MutableList<User> = userRepository.findAll()
 
     @ModelAttribute("roles")
     fun roles(): MutableIterable<Role> = roleRepository.findAll()
-
+    @ModelAttribute("users")
+    fun users(): MutableList<User> = userRepository.findAll()
+    @ModelAttribute("auth")
+    fun auth(auth: Authentication) = auth
+    @ModelAttribute("isAdmin")
+    fun isAdmin(auth: Authentication) = auth.authorities.any { it.authority == "ADMIN" }
     @ModelAttribute("onAdminPage")
     fun onAdminPage() = true
-
 
     @GetMapping("")
     fun admin(model : ModelMap, auth: Authentication) : String{
@@ -60,9 +56,6 @@ class AdminController {
 
     @GetMapping("/users/{id}/edit")
     fun editUser(@PathVariable id:Int, model: ModelMap, auth: Authentication) : String {
-        model["users"] = userRepository.findAll()
-        model["roles"] = roleRepository.findAll()
-        model["user"] = auth
         model["editUser"] = userRepository.findById(id).get()
         return "admin"
     }
