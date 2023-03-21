18eeaec436ad16a46d909f42ebc78e27cc9ea95d
EliasMorio <elias14160@gmail.com>
2023-03-21 08:10:57 +0100

    feat(User): add User methods entities

--- src/main/kotlin/edu/spring/btp/entities/User.kt
+++ src/main/kotlin/edu/spring/btp/entities/User.kt
@@ -31,4 +31,9 @@ open class User() {
     @ManyToMany(mappedBy = "claimants", cascade = [CascadeType.REMOVE])
     open var claims= mutableListOf<Complaint>()
 
+    @PreRemove
+    fun preRemove(){
+        complaints.forEach { it.user=null }
+    }
+
 }
