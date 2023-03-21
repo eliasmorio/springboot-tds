f12c4b7f960e4fe1094bc9d22e4681821960c7a6
EliasMorio <elias14160@gmail.com>
2023-03-21 10:00:30 +0100

    feat(complaint): see and add complaint for a domain

--- src/main/kotlin/edu/spring/btp/controllers/IndexController.kt
+++ src/main/kotlin/edu/spring/btp/controllers/IndexController.kt
@@ -1,19 +1,33 @@
 package edu.spring.btp.controllers
 
+import edu.spring.btp.entities.Complaint
+import edu.spring.btp.repositories.ComplaintRepository
 import edu.spring.btp.repositories.DomainRepository
+import edu.spring.btp.repositories.ProviderRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class IndexController {
 
     @Autowired
     lateinit var domainRepository: DomainRepository
+
+    @Autowired
+    lateinit var complaintRepository: ComplaintRepository
+
+    @Autowired
+    lateinit var providerRepository: ProviderRepository
 
 
 
@@ -32,15 +46,33 @@ class IndexController {
         return "index"
     }
 
-    @GetMapping("/complaints/{domain}")
-    fun complaints(@PathVariable domain: String, model: ModelMap): String {
-        val domain = domainRepository.findByName(domain)
+    @GetMapping("/complaints/{domainName}")
+    fun complaints(@PathVariable domainName: String, model: ModelMap): String {
+        if (domainName == "Root") return "redirect:/complaints"
+        val domain = domainRepository.findByName(domainName)
         model["domain"] = domain
-        model["complaints"] = domain.complaints
         return "complaints"
     }
 
+    @GetMapping("/complaints/{domainName}/new")
+    fun newComplaint(@PathVariable domainName: String, model: ModelMap): String {
+        val domain = domainRepository.findByName(domainName)
+        model["domain"] = domain
+        print(domain.providers)
+        return "forms/complaint"
+    }
 
+    @PostMapping("/complaints/{domainName}/new", consumes = ["application/x-www-form-urlencoded"])
+    fun storeComplaint(@PathVariable domainName: String,
+                       @RequestParam title : String,
+                       @RequestParam description : String,
+                       @RequestParam provider : Int): String {
+        val domain = domainRepository.findByName(domainName)
+        val provider = providerRepository.findById(provider).get()
+        val complaint = Complaint(title, description, null, provider, domain)
+        complaintRepository.save(complaint)
+        return "redirect:/complaints/$domainName"
+    }
 
 
 
