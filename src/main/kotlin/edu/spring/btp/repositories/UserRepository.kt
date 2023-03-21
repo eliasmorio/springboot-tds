6c9b2d7470b23cee5b5507a98b6b2092f65498d6
EliasMorio <elias14160@gmail.com>
2023-03-21 08:17:20 +0100

    feat(User): add method for user authentication

--- src/main/kotlin/edu/spring/btp/repositories/UserRepository.kt
+++ src/main/kotlin/edu/spring/btp/repositories/UserRepository.kt
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.Query
 interface UserRepository: JpaRepository<User, Int> {
     @Query(nativeQuery = true,value="SELECT * FROM \"user\" ORDER BY rand() LIMIT 1")
     fun getRandomUser(): User
+
+    @Query("SELECT u FROM User u WHERE u.username=:usernameOrEmail OR u.email=:usernameOrEmail")
+    fun findByUsernameOrEmail(usernameOrEmail: String?): User?
 }
